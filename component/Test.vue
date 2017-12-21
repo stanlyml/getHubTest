f2fb6f27d505984270f972628d3d86a8cac22a98
stanlyml <jiangzhuanximl@163.com>
2017-12-21 21:53:58 +0800

    添加一个测试文件
    加了一些东西

---
+++ component/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="">
+      test
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+    return {
+      msg:'helloe test '
+    }
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
