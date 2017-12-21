b96a80107c7c9f637d0c6d894589fbb295f36bbc
stanlyml <jiangzhuanximl@163.com>
2017-12-21 21:56:56 +0800

    添加一HElloe组件
    添加一个组件

---
+++ component/Helle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="">
+    helle vue !!!
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
